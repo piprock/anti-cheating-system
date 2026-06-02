6c5d58c5283b310f8a53876482d68c0ee34b619f
piprock <yura.losov@gmail.com>
2026-06-03 01:34:55 +0300

    Install system dependencies

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
+# Install system dependencies
+apt-get update && apt-get install -y portaudio19-dev
+
+# Install Python dependencies
 pip install -r requirements.txt
 
+# Run Django management commands
 python manage.py collectstatic --no-input
 python manage.py migrate
